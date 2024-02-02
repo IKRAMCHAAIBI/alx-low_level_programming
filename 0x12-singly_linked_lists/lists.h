2dc385e744a9870360fd3681ce9595778f249ecf
IKRAMCHAAIBI <chaaibi.ikrame@gmail.com>
2024-02-01 17:02:47 -0800

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,9 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
