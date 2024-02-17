c7fc7aecc70f1df72fd7b6dedd38807a5fa47820
IKRAMCHAAIBI <chaaibi.ikrame@gmail.com>
2024-02-16 16:32:43 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -87,7 +87,7 @@ int main(int argc, char *argv[])
 		}
 
 		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONGLY | O_APPEND);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 	}
 
 	while (r > 0);
